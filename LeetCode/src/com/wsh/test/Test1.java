ca847d46e6c5588c73a6bb7b90115f556d3ca907
wsh <wenshuaihuy@gmail.com>
2022-04-03 20:13:37 +0800

    找出一个有序数组中出现奇数次数的两个数

--- LeetCode/src/com/wsh/test/Test1.java
+++ LeetCode/src/com/wsh/test/Test1.java
@@ -5,6 +5,38 @@ package com.wsh.test;
  */
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("hello!!");
+//        System.out.println("hello!!");
+        int[] ints = {1,1,1,2,2,3,3,4};
+        printOddTimesNum(ints);
     }
+
+    /**
+     * 找出一个有序数组中出现奇数次数的两个数
+     */
+    public static void printOddTimesNum(int[] arr){
+        int ero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ero ^= arr[i];
+
+
+        }
+        //ero = a ^ b
+        //ero != 0
+        //提取出最右侧的1
+        int rightOne = ero & (~ero + 1);
+        //ero`
+        int onlyOne = 0;
+        for (int i1 : arr) {
+            if ((rightOne & i1) == 0){
+                onlyOne ^= i1;
+            }
+        }
+        System.out.println(onlyOne +"   " + (onlyOne^ero));
+
+
+
+    }
+
+
+
 }
