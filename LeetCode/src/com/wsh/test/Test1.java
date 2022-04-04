f7a76dab7e497c4509ad3a8389943d7d27ac93e2
wsh <wenshuaihuy@gmail.com>
2022-04-04 10:59:55 +0800

    找出一个有序数组中出现奇数次数的一个数

--- LeetCode/src/com/wsh/test/Test1.java
+++ LeetCode/src/com/wsh/test/Test1.java
@@ -5,20 +5,33 @@ package com.wsh.test;
  */
 public class Test1 {
     public static void main(String[] args) {
-//        System.out.println("hello!!");
-        int[] ints = {1,1,1,2,2,3,3,4};
-        printOddTimesNum(ints);
+        int[] ints1 = {1, 1, 2, 2, 3, 3, 6};
+        int[] ints2 = {1, 1, 1, 2, 2, 3, 3, 4};
+        printOddTimesNum1(ints1);
+        printOddTimesNum2(ints2);
     }
+
+
+    /**
+     * 找出一个有序数组中出现奇数次数的一个数
+     */
+    public static void printOddTimesNum1(int[] arr) {
+        int ero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ero ^= arr[i];
+        }
+        System.out.println(ero);
+
+    }
+
 
     /**
      * 找出一个有序数组中出现奇数次数的两个数
      */
-    public static void printOddTimesNum(int[] arr){
+    public static void printOddTimesNum2(int[] arr) {
         int ero = 0;
         for (int i = 0; i < arr.length; i++) {
             ero ^= arr[i];
-
-
         }
         //ero = a ^ b
         //ero != 0
@@ -27,16 +40,12 @@ public class Test1 {
         //ero`
         int onlyOne = 0;
         for (int i1 : arr) {
-            if ((rightOne & i1) == 0){
+            if ((rightOne & i1) == 0) {
                 onlyOne ^= i1;
             }
         }
-        System.out.println(onlyOne +"   " + (onlyOne^ero));
-
-
-
+        System.out.println(onlyOne + "   " + (onlyOne ^ ero));
     }
-
 
 
 }
