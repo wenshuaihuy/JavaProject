595b99fc78e3bf119f2e15ac24bd242aac07cae3
wsh <wenshuaihuy@gmail.com>
2022-04-10 21:17:51 +0800

    堆排序

--- LeetCode/src/com/wsh/test/Test2.java
+++ LeetCode/src/com/wsh/test/Test2.java
@@ -10,7 +10,7 @@ public class Test2 {
     }
 
     /**
-     * 新进来的数与其父节点去对比，如果比父节点的数大就进行交换保证父节点最大
+     * 新进来的数与其父节点去对比，如果比父节点的数大就进行交换保证父节点最大（从下往上换）时间复杂度o(logN)
      * @param arr
      * @param index
      */
@@ -23,6 +23,57 @@ public class Test2 {
             index = fatherPort;
         }
     }
+
+    /**
+     * 反悔堆中的最大值，并移除完最大值，之后保证这是一个大根堆（从上往下换）时间复杂度o(logN)
+     * @param arr 堆数组
+     * @param index 从哪个位置开始往下走
+     * @param heapSize 最大的长度 保证左右孩子下标不越界
+     */
+    public static void heapify(int[] arr, int index, int heapSize) {
+        //左孩子的下标
+        int left = (index * 2) + 1;
+        while (left < heapSize) {
+            //两个孩子中谁的值大就把谁的下标给largest
+            int largest = (left + 1 < heapSize) && (arr[left + 1] > arr[left]) ? left + 1 : left;
+            //父亲和最大的孩子比较谁的大就把下标赋值给谁
+            largest = arr[index] > arr[largest] ? index : largest;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, largest, index);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+
+    /**
+     * 堆排序
+     * @param arr
+     */
+    public static void heapsort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            //将堆变成大顶堆
+            heapInsert(arr, i);
+        }
+
+        int heapSize = arr.length;
+        swap(arr,0,heapSize-1);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr,0,--heapSize);
+
+
+        }
+
+
+    }
+
+
 
     /**
      * 交换方法
