161bfeb29095dd7c5d93d369b47b5e19713775ee
wsh <wenshuaihuy@gmail.com>
2022-05-08 14:18:15 +0800

    桶排序

--- LeetCode/src/com/wsh/test/Test2.java
+++ LeetCode/src/com/wsh/test/Test2.java
@@ -82,4 +82,40 @@ public class Test2 {
         arr[l] = arr[r];
         arr[r] = a;
     }
+
+    public static int getDigit(int x, int d) {
+        return ((x / ((int) Math.pow(10, d - 1))) % 10);
+    }
+
+    /**
+     * 桶排序
+     * @param arr
+     * @param begin
+     * @param end
+     * @param digit
+     */
+    public static void radixSort(int[] arr, int begin, int end, int digit) {
+        final int radix = 10;
+        int i = 0, j = 0;
+
+        int[] bucket = new int[end - begin + 1];
+        for (int d = 1; d <= digit; d++) {
+            int[] count = new int[radix];
+            for (i = begin; i <= end; i++) {
+                j = getDigit(arr[i], d);
+                count[j]++;
+            }
+            for (i = 1; i < radix; i++) {
+                count[i] = count[i] + count[i - 1];
+            }
+            for (i = end; i >= begin; i--) {
+                j = getDigit(arr[i], d);
+                bucket[count[j] - 1] = arr[i];
+                count[j]--;
+            }
+            for (i = begin, j = 0; i <= end; i++, j++) {
+                arr[i] = bucket[j];
+            }
+        }
+    }
 }
