a9699fb2b46e709ee2d121fe4381cdbf153cd2aa
wsh <wenshuaihuy@gmail.com>
2022-05-08 21:20:45 +0800

    链表

---
+++ LeetCode/src/com/wsh/test/Node.java
@@ -0,0 +1,14 @@
+package com.wsh.test;
+
+/**
+ * @author wsh
+ * @date 2022/5/8 8:25 下午
+ */
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
---
+++ LeetCode/src/com/wsh/test/Test3.java
@@ -0,0 +1,10 @@
+package com.wsh.test;
+
+/**
+ * @author wsh
+ * @date 2022/5/8 8:20 下午
+ */
+public class Test3 {
+    //反转单向链表
+    //反转双向链表
+}
