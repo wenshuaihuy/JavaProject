f6d6c6968ab2ba7d7f80e0e2dab7ad9eaff02d28
wsh <wenshuaihuy@gmail.com>
2022-05-10 19:39:06 +0800

    链表 添加数据

--- LeetCode/src/com/wsh/test/Node.java
+++ LeetCode/src/com/wsh/test/Node.java
@@ -5,10 +5,27 @@ package com.wsh.test;
  * @date 2022/5/8 8:25 下午
  */
 public class Node {
-    public int value;
+    /**
+     *数据
+     */
+    public int data;
+    /**
+     *节点
+     */
     public Node next;
 
     public Node(int data) {
-        this.value = data;
+        this.data = data;
     }
+
+    private void add(int data) {
+        //如何当前节点的下一个数据是空的话就进行data赋值
+        if (this.next == null) {
+            this.next = new Node(data);
+        } else { //否则递归 去查看下一个数据节点是不是空
+            this.next.add(data);
+        }
+
+    }
+
 }
